31a3ac75d823a41a574cc39efd64e10cce0508ac
cameron2024 <153026033+cameron2024@users.noreply.github.com>
2026-05-27 13:40:05 -0700

    Add files via upload

--- sidebar.js
+++ sidebar.js
@@ -65,7 +65,7 @@
       Discounted Cash Flow
     </a>
     <a class="nav-item" href="practice-screen.html?topic=Mergers_MA">
-      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"><path d="M3 5l4 4v6l5 5"/><path d="M21 5l-4 4v6l-5 5"/><path d="M5 7l-2-2 2-2"/><path d="M19 7l2-2-2-2"/></svg>
+      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"><path d="m11 17 2 2a1 1 0 1 0 3-3"/><path d="m14 14 2.5 2.5a1 1 0 1 0 3-3l-3.88-3.88a3 3 0 0 0-4.24 0l-.88.88a1 1 0 1 1-3-3l2.81-2.81a5.79 5.79 0 0 1 7.06-.87l.47.28a2 2 0 0 0 1.42.25L21 4"/><path d="m21 3 1 11h-2"/><path d="M3 3 2 14l6.5 6.5a1 1 0 1 0 3-3"/><path d="M3 4h8"/></svg>
       Mergers &amp; Acquisitions
     </a>
     <a class="nav-item" href="practice-screen.html?topic=LBO">
